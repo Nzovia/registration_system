d4654b6862285bc856d626f8f5da70d4304d3e43
Nicholas <nickiemaundu19@gmail.com>
2022-05-16 11:53:21 +0300

    added app user class and the Roles enum

---
+++ src/main/java/com/example/registration_system/appUser/AppUser.java
@@ -0,0 +1,66 @@
+package com.example.registration_system.appUser;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_data")
+public class AppUser implements UserDetails {
+    @Id
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private AppUserRole appUserRole;
+    private boolean locked;
+    private boolean enabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(appUserRole.name());
+        return Collections.singletonList(simpleGrantedAuthority);
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/main/java/com/example/registration_system/appUser/AppUserRole.java
@@ -0,0 +1,7 @@
+package com.example.registration_system.appUser;
+
+public enum AppUserRole {
+    //enum is a special type of class that represents a group of constants
+    USER,
+    ADMIN
+}
